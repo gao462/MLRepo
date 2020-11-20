Fix corner cases of computation IO flow logging.

demo/Reproduce/models/linseq.py
@@ -16,9 +16,7 @@ from typing import cast
 # Import dependencies.
 import sys
 import os
-import abc
 import torch
-from torch_geometric.nn import GINConv # type: ignore
 
 # Add development library to path.
 if (os.path.basename(os.getcwd()) == "MLRepo"):
@@ -31,35 +29,57 @@ else:
 from pytorch.logging import debug, info1, info2, focus, warning, error
 
 # Import dependencies.
-from pytorch.models.model import GradModel
-from pytorch.models.model import Parameter, ForwardFunction, NullFunction
-from pytorch.models.graph.update import Update
+from pytorch.models.model import GradModel, Parameter
+from pytorch.models.model import ForwardFunction, LossFunction, NullFunction
 from pytorch.models.linear import Linear
-from pytorch.models.graph.msgpass import MessagePass
-from pytorch.models.graph.message import ConcatMessage
-from pytorch.models.graph.aggregate import SumNeighborAgg
+from pytorch.models.collect import GradModelSeq
+from demo.Reproduce.models.naive import NaiveDistLoss
 
 
 # =============================================================================
 # *****************************************************************************
 # -----------------------------------------------------------------------------
-# << GIN Gradient Model >>
-# The GIN gradient model.
-# This is implementation of "Graph Isomorphism Network".
+# << Linear Gradient Model Sequence Naive Distribution >>
 # -----------------------------------------------------------------------------
 # *****************************************************************************
 # =============================================================================
 
 
-class GINUpdate(Update):
+class RepLinearSeq(GradModel):
     r"""
-    GIN update.
+    Reproduced Linear sequence.
     """
     # Define main flow name.
-    main = "gin_update"
+    main = "linear_seq"
+
+    def __parse__(
+        self: RepLinearSeq,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> None:
+        r"""
+        Parse computation IO keys.
+
+        Args
+        ----
+        - self
+        - *args
+        - **kargs
+
+        Returns
+        -------
+
+        """
+        # /
+        # ANNOTATE VARIABLES
+        # /
+        ...
+
+        # No flow runs.
+        pass
 
     def __forward__(
-        self: GINUpdate,
+        self: RepLinearSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> ForwardFunction:
@@ -117,29 +137,15 @@ class GINUpdate(Update):
             # /
             output: Dict[str, torch.Tensor]
 
-            # Get epsilon scaled update.
-            eps = 0
-            tensor = (1 + eps) * input[self.ky_input_v] + input[self.ky_agg]
-
-            # Transfer it by dual linear
-            transient = {self.ky_output: tensor}
-            transient = self.linear1.forward(
-                parameter.sub("linear1"), transient,
-            )
-            transient[self.ky_output] = getattr(torch.nn.functional, "relu")(
-                transient[self.ky_output],
-            )
-            transient = self.linear2.forward(
-                parameter.sub("linear2"), transient,
-            )
-            output = {self.ky_output: transient[self.ky_output]}
+            # Apply sequential flow directly.
+            output = self.seq.forward(parameter.sub("seq"), input)
             return output
 
         # Return the function.
         return f
 
     def __nullin__(
-        self: GINUpdate,
+        self: RepLinearSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> NullFunction:
@@ -195,17 +201,14 @@ class GINUpdate(Update):
             # /
             ...
 
-            # Get linear inputs applied on destination and aggregation.
-            return {
-                self.ky_input_v: self.linear1.nullin(device)[self.ky_output],
-                self.ky_agg: self.linear1.nullin(device)[self.ky_output],
-            }
+            # return all-zero.
+            return self.seq.nullin(device)
 
         # Return the function.
         return null
 
     def __nullout__(
-        self: GINUpdate,
+        self: RepLinearSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> NullFunction:
@@ -261,15 +264,14 @@ class GINUpdate(Update):
             # /
             ...
 
-
-            # Get linear input directly.
-            return self.linear2.nullout(device)
+            # return all-zero.
+            return self.seq.nullout(device)
 
         # Return the function.
         return null
 
     def configure(
-        self: GINUpdate,
+        self: RepLinearSeq,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
         *args: ArgT,
         **kargs: KArgT,
@@ -300,13 +302,12 @@ class GINUpdate(Update):
         self.num_inputs = xkargs["num_inputs"]
         self.num_outputs = xkargs["num_outputs"]
 
-        # Create dual linear layers.
-        _, (key,) = self.IOKEYS["gin_update"]
-        self.linear1 = Linear(
+        # Create a dual linear.
+        linear1 = Linear(
             self.ROOT,
             sub=True, dtype=self.DTYPE_NAME, iokeys=dict(
-                linear=([key], [key]),
-            )
+                linear=(["input"], ["input1"]),
+            ),
         ).set(
             xargs=(),
             xkargs=dict(
@@ -314,11 +315,11 @@ class GINUpdate(Update):
                 no_bias=False,
             ),
         )
-        self.linear2 = Linear(
+        linear2 = Linear(
             self.ROOT,
             sub=True, dtype=self.DTYPE_NAME, iokeys=dict(
-                linear=([key], [key]),
-            )
+                linear=(["input1"], ["output"]),
+            ),
         ).set(
             xargs=(),
             xkargs=dict(
@@ -327,244 +328,17 @@ class GINUpdate(Update):
             ),
         )
 
-    def __initialize__(
-        self: GINUpdate,
-        rng: torch._C.Generator,
-        *args: ArgT,
-        xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
-        **kargs: KArgT,
-    ) -> None:
-        r"""
-        Initialize model parameters and sub models.
-
-        Args
-        ----
-        - self
-        - rng
-            Random number generator.
-        - *args
-        - xargs
-            Extra arguments to specific initialization.
-        - xkargs
-            Extra keyword arguments to specific initialization.
-        - **kargs
-
-        Returns
-        -------
-
-        """
-        # \
-        # ANNOTATE VARIABLES
-        # \
-        ...
-
-        # Initialize recursively.
-        self.linear1.initialize(
-            rng,
-            xargs=xkargs["bilin"]["xargs"], xkargs=xkargs["bilin"]["xkargs"],
-        )
-        self.linear2.initialize(
-            rng,
-            xargs=xkargs["bilin"]["xargs"], xkargs=xkargs["bilin"]["xkargs"],
-        )
-
-
-class GIN(MessagePass):
-    r"""
-    GIN.
-    """
-    # Define main flow name.
-    main = "gin"
-
-    def __nullin__(
-        self: GIN,
-        *args: ArgT,
-        **kargs: KArgT,
-    ) -> NullFunction:
-        r"""
-        Generate null input.
-
-        Args
-        ----
-        - self
-        - *kargs
-        - **kargs
-
-        Returns
-        -------
-        - null
-            Null input generation function.
-
-        After setup, the null input to a model is fixed.
-        This is helpful to understand the expectation of input.
-        This is also useful when the default model output is required, for
-        example, this is the sub model of RNN H-to-H model at the first time
-        step.
-        It has batch size 1 for robustness.
-        """
-        # /
-        # ANNOTATE VARIABLES
-        # /
-        ...
-
-        def null(
-            device: str,
-            *args: ArgT,
-            **kargs: KArgT,
-        ) -> Dict[str, torch.Tensor]:
-            r"""
-            Generate null input on device.
-
-            Args
-            ----
-            - device
-                Device.
-            - *args
-            - **kargs
-
-            Returns
-            -------
-            - output
-                Output.
-
-            """
-            # /
-            # ANNOTATE VARIABLES
-            # /
-            ...
-
-            # GIN has no null definition.
-            raise NotImplementedError
-
-        # Return the function.
-        return null
-
-    def __nullout__(
-        self: GIN,
-        *args: ArgT,
-        **kargs: KArgT,
-    ) -> NullFunction:
-        r"""
-        Generate null output.
-
-        Args
-        ----
-        - self
-        - *kargs
-        - **kargs
-
-        Returns
-        -------
-        - null
-            Null output generation function.
-
-        After setup, the null output to a model is fixed.
-        This is helpful to understand the expectation of output.
-        This is also useful when the default model output is required, for
-        example, this is the sub model of RNN H-to-H model at the first time
-        step.
-        It has batch size 1 for robustness.
-        """
-        # /
-        # ANNOTATE VARIABLES
-        # /
-        ...
-
-        def null(
-            device: str,
-            *args: ArgT,
-            **kargs: KArgT,
-        ) -> Dict[str, torch.Tensor]:
-            r"""
-            Generate null output on device.
-
-            Args
-            ----
-            - device
-                Device.
-            - *args
-            - **kargs
-
-            Returns
-            -------
-            - output
-                Output.
-
-            """
-            # /
-            # ANNOTATE VARIABLES
-            # /
-            ...
-
-            # Get linear input directly.
-            return self.update.nullout(device)
-
-        # Return the function.
-        return null
-
-    def configure(
-        self: GIN,
-        xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
-        *args: ArgT,
-        **kargs: KArgT,
-    ) -> None:
-        r"""
-        Configure model.
-
-        Args
-        ----
-        - self
-        - xargs
-            Extra arguments to specific configuration.
-        - xkargs
-            Extra keyword arguments to specific configuration.
-        - *args
-        - **kargs
-
-        Returns
-        -------
-
-        """
-        # \
-        # ANNOTATE VARIABLES
-        # \
-        ...
-
-        # Save necessary attributes.
-        self.num_inputs = xkargs["num_inputs"]
-        self.num_outputs = xkargs["num_outputs"]
-
-        # Create message layers.
-        self.message = ConcatMessage(
+        # Allocate sequence collection.
+        self.seq = cast(GradModelSeq, GradModelSeq(
             self.ROOT,
-            sub=True, dtype=self.DTYPE_NAME, iokeys=dict(
-                concat_msg=self.IOKEYS["{:s}_msg".format(self.main)],
-            )
-        ).set(xargs=(), xkargs=dict())
-
-        # Create aggregation layers.
-        self.aggregate = SumNeighborAgg(
-            self.ROOT,
-            sub=True, dtype=self.DTYPE_NAME, iokeys=dict(
-                sum_neighbor_agg=self.IOKEYS["{:s}_agg".format(self.main)],
-            )
-        ).set(xargs=(), xkargs=dict())
-
-        # Create update layers.
-        self.update = GINUpdate(
-            self.ROOT,
-            sub=True, dtype=self.DTYPE_NAME, iokeys=dict(
-                gin_update=self.IOKEYS[self.main],
-            )
+            sub=True, dtype=self.DTYPE_NAME, iokeys={},
         ).set(
-            xargs=(),
-            xkargs=dict(
-                num_inputs=self.num_inputs, num_outputs=self.num_outputs,
-            ),
-        )
+            xargs=(linear1, linear2),
+            xkargs=dict(),
+        ))
 
     def __initialize__(
-        self: GIN,
+        self: RepLinearSeq,
         rng: torch._C.Generator,
         *args: ArgT,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
@@ -589,23 +363,87 @@ class GIN(MessagePass):
         -------
 
         """
-        # \
+        # /
         # ANNOTATE VARIABLES
-        # \
+        # /
         ...
 
-        # Initialize recursively.
-        self.message.initialize(rng, xargs=xargs, xkargs=xkargs)
-        self.aggregate.initialize(rng, xargs=xargs, xkargs=xkargs)
-        self.update.initialize(rng, xargs=xargs, xkargs=xkargs)
+        # Initialize sequence.
+        self.seq.initialize(rng, xargs=xargs, xkargs=xkargs)
+
+    def __train__(
+        self: RepLinearSeq,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> LossFunction:
+        r"""
+        Set training loss function.
+
+        Args
+        ----
+        - self
+        - *args
+        - **kargs
+
+        Returns
+        -------
+        - function
+            Forward function.
+
+        It must of defined a function $f$ by form:
+        $$
+        l = f(\theta, y, \hat{y})
+        $$
+        where $\theta$ is parameter, $y$ is output, and $\hat{y}$ is target.
+        """
+        # /
+        # ANNOTATE VARIABLES
+        # /
+        ...
+
+        # return the function.
+        return NaiveDistLoss((["output"], ["target"]))
+
+    def __evaluate__(
+        self: RepLinearSeq,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> LossFunction:
+        r"""
+        Set evaluating loss function.
+
+        Args
+        ----
+        - self
+        - *args
+        - **kargs
+
+        Returns
+        -------
+        - function
+            Forward function.
+
+        It must of defined a function $f$ by form:
+        $$
+        l = f(\theta, y, \hat{y})
+        $$
+        where $\theta$ is parameter, $y$ is output, and $\hat{y}$ is target.
+        """
+        # /
+        # ANNOTATE VARIABLES
+        # /
+        ...
+
+        # Return the function.
+        return NaiveDistLoss((["output"], ["target"]))
 
 
-class __GIN__(GIN):
+class TarLinearSeq(RepLinearSeq):
     r"""
-    PyTorch GIN.
+    Targeting linear sequence.
     """
     def __forward__(
-        self: __GIN__,
+        self: TarLinearSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> ForwardFunction:
@@ -624,11 +462,9 @@ class __GIN__(GIN):
             Forward function.
 
         It must of defined a function $f$ by form:
-
         $$
         y = f(\theta, x)
         $$
-
         where $y$ is output, $\theta$ is parameter and $x$ is input.
         """
         # /
@@ -665,18 +501,16 @@ class __GIN__(GIN):
             # /
             output: Dict[str, torch.Tensor]
 
-            # Compute directly.
+            # Apply sequential flow directly.
             output = {}
-            output[self.ky_output] = self.pytorch.forward(
-                input[self.ky_input_v], input[self.ky_adj],
-            )
+            output["output"] = getattr(self.pytorch, "forward")(input["input"])
             return output
 
         # Return the function.
         return f
 
     def configure(
-        self: __GIN__,
+        self: TarLinearSeq,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
         *args: ArgT,
         **kargs: KArgT,
@@ -703,37 +537,27 @@ class __GIN__(GIN):
         # \
         ...
 
-        # Save necessary attributes.
-        self.num_inputs = xkargs["num_inputs"]
-        self.num_outputs = xkargs["num_outputs"]
-
         # Super.
-        GIN.configure(self, xargs, xkargs)
+        RepLinearSeq.configure(self, xargs, xkargs)
 
-        # Allocate parameters.
-        self.pytorch = GINConv(
-            torch.nn.Sequential(
-                torch.nn.Linear(
-                    in_features=self.num_inputs,
-                    out_features=self.num_outputs,
-                    bias=True,
-                ),
-                torch.nn.ReLU(),
-                torch.nn.Linear(
-                    in_features=self.num_outputs,
-                    out_features=self.num_outputs,
-                    bias=True,
-                ),
+        # Allocate sequence collection.
+        self.pytorch = torch.nn.Sequential(
+            torch.nn.Linear(
+                in_features=self.num_inputs, out_features=self.num_outputs,
+                bias=True,
             ),
-            eps=0, train_eps=False,
+            torch.nn.Linear(
+                in_features=self.num_outputs, out_features=self.num_outputs,
+                bias=True,
+            ),
         )
-        self.weight1 = self.pytorch.nn[0].weight
-        self.weight2 = self.pytorch.nn[2].weight
-        self.bias1 = self.pytorch.nn[0].bias
-        self.bias2 = self.pytorch.nn[2].bias
+        self.weight1 = self.pytorch[0].weight
+        self.weight2 = self.pytorch[1].weight
+        self.bias1 = self.pytorch[0].bias
+        self.bias2 = self.pytorch[1].bias
 
     def __initialize__(
-        self: __GIN__,
+        self: TarLinearSeq,
         rng: torch._C.Generator,
         *args: ArgT,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
@@ -764,19 +588,14 @@ class __GIN__(GIN):
         ...
 
         # Super.
-        GIN.__initialize__(self, rng, xargs=xargs, xkargs=xkargs)
+        RepLinearSeq.__initialize__(self, rng, xargs=xargs, xkargs=xkargs)
 
-        # Copy data to correct place.
-        self.update = cast(GINUpdate, self.update)
-        self.weight1.data.copy_(cast(Linear, self.update.linear1).weight.data)
-        self.weight2.data.copy_(cast(Linear, self.update.linear2).weight.data)
-        self.bias1.data.copy_(cast(Linear, self.update.linear1).bias.data)
-        self.bias2.data.copy_(cast(Linear, self.update.linear2).bias.data)
+        # Copy data.
+        self.weight1.data.copy_(cast(Linear, self.seq[0]).weight.data)
+        self.weight2.data.copy_(cast(Linear, self.seq[1]).weight.data)
+        self.bias1.data.copy_(cast(Linear, self.seq[0]).bias.data)
+        self.bias2.data.copy_(cast(Linear, self.seq[1]).bias.data)
 
-        # Remove from registration.
-        del self.parameter.submodels["message"]
-        del self.parameter.submodels["aggregate"]
-        del self.parameter.submodels["update"]
-        del self.message
-        del self.aggregate
-        del self.update
+        # Remove from registration
+        del self.parameter.submodels["seq"]
+        del self.seq

pytorch/models/collect.py
@@ -38,24 +38,22 @@ from pytorch.models.model import Parameter, ForwardFunction, NullFunction
 # =============================================================================
 # *****************************************************************************
 # -----------------------------------------------------------------------------
-# << Linear Gradient Model >>
-# The linear gradient model.
-# The model forward function $f$ must be constructed with linear matrix
-# operations, such as matrix multiplication, element-wise addition.
+# << Collection of Gradient Model >>
+# The collection of gradient models working as a gradient model.
 # -----------------------------------------------------------------------------
 # *****************************************************************************
 # =============================================================================
 
 
-class Linear(GradModel):
+class GradModelSeq(GradModel):
     r"""
-    Linear.
+    A sequence of gradient models.
     """
     # Define main flow name.
-    main = "linear"
+    main = "seq"
 
     def __parse__(
-        self: Linear,
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> None:
@@ -75,14 +73,13 @@ class Linear(GradModel):
         # /
         # ANNOTATE VARIABLES
         # /
-        self.ky_input: str
-        self.ky_output: str
+        ...
 
-        # Fetch main input and output.
-        (self.ky_input,), (self.ky_output,) = self.IOKEYS[self.main]
+        # Collection is just a wrapped without IO flow.
+        pass
 
     def __forward__(
-        self: Linear,
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> ForwardFunction:
@@ -141,21 +138,19 @@ class Linear(GradModel):
             output: Dict[str, torch.Tensor]
 
             # Apply matrix multiplication and bias.
-            output = {}
-            tensor = getattr(torch, "matmul")(
-                input[self.ky_input], parameter["weight"].t(),
-            )
-            if (self.no_bias):
-                output[self.ky_output] = tensor
-            else:
-                output[self.ky_output] = tensor + parameter["bias"]
+            transient = input
+            for i, model in enumerate(self.models):
+                transient = model.forward(
+                    parameter.sub("{:d}".format(i)), transient,
+                )
+            output = transient
             return output
 
         # Return the function.
         return f
 
     def __nullin__(
-        self: Linear,
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> NullFunction:
@@ -212,17 +207,13 @@ class Linear(GradModel):
             ...
 
             # return all-zero.
-            return {
-                self.ky_input: getattr(torch, "zeros")(
-                    1, self.num_inputs, dtype=self.DTYPE, device=device,
-                ),
-            }
+            return self.models[0].nullin(device)
 
         # Return the function.
         return null
 
     def __nullout__(
-        self: Linear,
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> NullFunction:
@@ -279,17 +270,13 @@ class Linear(GradModel):
             ...
 
             # return all-zero.
-            return {
-                self.ky_output: getattr(torch, "zeros")(
-                    1, self.num_outputs, dtype=self.DTYPE, device=device,
-                ),
-            }
+            return self.models[-1].nullout(device)
 
         # Return the function.
         return null
 
     def configure(
-        self: Linear,
+        self: GradModelSeq,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
         *args: ArgT,
         **kargs: KArgT,
@@ -314,30 +301,55 @@ class Linear(GradModel):
         # \
         # ANNOTATE VARIABLES
         # \
-        self.weight: torch.nn.parameter.Parameter
-        self.bias: torch.nn.parameter.Parameter
-        self.num_inputs: int
-        self.num_outputs: int
-        self.no_bias: bool
+        self.models: List[GradModel]
 
         # Save necessary attributes.
-        self.num_inputs = xkargs["num_inputs"]
-        self.num_outputs = xkargs["num_outputs"]
-        self.no_bias = xkargs["no_bias"]
+        self.models = []
+        for model in xargs:
+            self.models.append(model)
 
-        # Allocate parameters.
-        self.weight = torch.nn.parameter.Parameter(getattr(torch, "zeros")(
-            self.num_outputs, self.num_inputs, dtype=self.DTYPE,
-        ))
-        if (self.no_bias):
-            pass
-        else:
-            self.bias = torch.nn.parameter.Parameter(getattr(torch, "zeros")(
-                self.num_outputs, dtype=self.DTYPE,
-            ))
+    def workflow_diffuse(
+        self: GradModelSeq,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> Dict[str, List[List[Tuple[str, str, str]]]]:
+        r"""
+        Diffuse workflow defined by sub models and IO keys.
+
+        Args
+        ----
+        - self
+        - *args
+        - **kargs
+
+        Returns
+        -------
+        - flows.
+            Work flows of each sub model.
+            The model itself is defined as sub model "".
+            Work flow is a list of lists of section, input key and output key
+            items.
+            Same section name is aggregated in the same list.
+
+        """
+        # \
+        # ANNOTATE VARIABLES
+        # \
+        flows: Dict[str, List[List[Tuple[str, str, str]]]]
+
+        # Get sub model flows.
+        flows = {}
+        for i, val in enumerate(self.models):
+            key = "{:d}".format(i)
+            for sub, grouped in val.workflow.items():
+                if (sub == ""):
+                    flows[key] = grouped
+                else:
+                    flows[key + "." + sub] = grouped
+        return flows
 
     def __initialize__(
-        self: Linear,
+        self: GradModelSeq,
         rng: torch._C.Generator,
         *args: ArgT,
         xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
@@ -369,81 +381,18 @@ class Linear(GradModel):
         ...
 
         # Initialize weight.
-        gain = Linear.activation_gain(
-            name=xkargs["activation"], negative_slope=xkargs["negative_slope"],
-        )
-        std = gain / math.sqrt(self.num_inputs)
-        bound = math.sqrt(3) * std
-        self.weight.data.uniform_(-bound, bound, generator=rng)
+        for i, ini in enumerate(xargs):
+            self.models[i].initialize(
+                rng, xargs=ini["xargs"], xkargs=ini["xkargs"],
+            )
 
-        # Initialize bias if necessary.
-        if (self.no_bias):
-            pass
-        else:
-            bound = 1 / math.sqrt(self.num_inputs)
-            self.bias.data.uniform_(-bound, bound, generator=rng)
-
-    @staticmethod
-    def activation_gain(
-        *args: ArgT,
-        name: str, negative_slope: float,
-        **kargs: KArgT,
-    ) -> float:
-        r"""
-        Activation gain for initialization.
-
-        Args
-        ----
-        - *args
-        - name
-            Activation name.
-            "null" for no activation or convolution.
-            "sigmoid" for Sigmoid activation.
-            "tanh" for Tanh activation.
-            "relu" for Relu activation.
-            "leaky_relu" for Leaky ReLU activation.
-        - negative_slope
-            Negative slope for Leaky ReLU activation.
-            Use 0 if activation is not Leaky ReLU.
-        - **kargs
-
-        Returns
-        -------
-        - gain
-            Gain.
-
-        It is directly duplicated from PyTorch default.
-        """
-        # /
-        # ANNOTATE VARIABLES
-        # /
-        ...
-
-        # Get gain.
-        if (name == "sigmoid"):
-            return 1.0
-        elif (name == "tanh"):
-            return 5.0 / 3.0
-        elif (name == "relu"):
-            return math.sqrt(2.0)
-        elif (name == "leaky_relu"):
-            return math.sqrt(2.0 / (1.0 + negative_slope ** 2))
-        else:
-            error("Activcation \"{:s}\" has no gain definition.", name)
-            raise RuntimeError
-
-
-class __Linear__(Linear):
-    r"""
-    PyTorch Linear.
-    """
-    def __forward__(
-        self: __Linear__,
+    def __len__(
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
-    ) -> ForwardFunction:
+    ) -> int:
         r"""
-        Set forward function.
+        Get length.
 
         Args
         ----
@@ -453,75 +402,56 @@ class __Linear__(Linear):
 
         Returns
         -------
-        - function
-            Forward function.
 
-        It must of defined a function $f$ by form:
-
-        $$
-        y = f(\theta, x)
-        $$
-
-        where $y$ is output, $\theta$ is parameter and $x$ is input.
         """
-        # /
+        # \
         # ANNOTATE VARIABLES
-        # /
+        # \
         ...
 
-        def f(
-            parameter: Parameter,
-            input: Dict[str, torch.Tensor],
-            *args: ArgT,
-            **kargs: KArgT,
-        ) -> Dict[str, torch.Tensor]:
-            r"""
-            Forward a batch input.
+        # Get memory length.
+        return len(self.models)
 
-            Args
-            ----
-            - parameter
-                Parameter.
-            - input
-                Input.
-            - *args
-            - **kargs
+    def __getitem__(
+        self: GradModelSeq,
+        i: int,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> GradModel:
+        r"""
+        Get length.
 
-            Returns
-            -------
-            - output
-                Output.
+        Args
+        ----
+        - self
+        - i
+            Index.
+        - *args
+        - **kargs
 
-            """
-            # /
-            # ANNOTATE VARIABLES
-            # /
-            output: Dict[str, torch.Tensor]
+        Returns
+        -------
 
-            # Apply matrix multiplication and bias.
-            output = {}
-            output[self.ky_output] = self.pytorch.forward(input[self.ky_input])
-            return output
+        """
+        # \
+        # ANNOTATE VARIABLES
+        # \
+        ...
 
-        # Return the function.
-        return f
+        # Get item from saved models.
+        return self.models[i]
 
-    def configure(
-        self: __Linear__,
-        xargs: Tuple[Naive, ...], xkargs: Dict[str, Naive],
+    def register(
+        self: GradModelSeq,
         *args: ArgT,
         **kargs: KArgT,
     ) -> None:
         r"""
-        Configure model.
+        Register parameters and sub models.
 
         Args
         ----
         - self
-        - xargs
-            Extra arguments to specific configuration.
-        - xkargs
-            Extra keyword arguments to specific configuration.
         - *args
         - **kargs
 
@@ -529,19 +459,16 @@ class __Linear__(Linear):
         -------
 
         """
-        # \
+        # /
         # ANNOTATE VARIABLES
-        # \
-        self.weight: torch.nn.parameter.Parameter
-        self.bias: torch.nn.parameter.Parameter
+        # /
+        ...
 
         # Super.
-        Linear.configure(self, xargs, xkargs)
+        GradModel.register(self)
 
-        # Allocate parameters.
-        self.pytorch = torch.nn.Linear(
-            in_features=self.num_inputs, out_features=self.num_outputs,
-            bias=not self.no_bias,
-        )
-        self.weight = getattr(self.pytorch, "weight")
-        self.bias = getattr(self.pytorch, "bias")
+        # Manually register the model list.
+        for i, model in enumerate(self.models):
+            self.parameter.registar_submodel(
+                "{:d}".format(i), model.parameter,
+            )

pytorch/models/model.py
@@ -552,22 +552,8 @@ class GradModel(abc.ABC):
             )
             self.workflow_logging()
 
-        # Scan over model attributes by now.
-        self.parameter = Parameter()
-        for key, val in vars(self).items():
-            if (isinstance(val, torch.nn.parameter.Parameter)):
-                self.parameter.registar_parameter(key, val)
-            elif (isinstance(val, GradModel)):
-                if (val.SUB):
-                    self.parameter.registar_submodel(key, val.parameter)
-                else:
-                    error(
-                        "Register a non-sub model \"{:s}\"as a sub model.",
-                        val.__class__.__name__,
-                    )
-                    raise RuntimeError
-            else:
-                pass
+        # Scan over model attributes by now and register them.
+        self.register()
 
         # Construct purified function.
         self.forward = self.__forward__()
@@ -696,6 +682,17 @@ class GradModel(abc.ABC):
             else:
                 highlight = ""
 
+            # Defined no-self-flow need special logging.
+            if (subname == "" and len(submodel) == 0):
+                debug(
+                    "\" {:s} {:s} | {:s} --- {:s} \".",
+                    subname.rjust(max_sub), "".rjust(max_sec),
+                    "-".rjust(max_in), "-".ljust(max_out),
+                    highlight=highlight,
+                )
+            else:
+                pass
+
             # Output each defined flow.
             for j, grouped in enumerate(submodel):
                 # Output subname only at the beginning.
@@ -706,13 +703,22 @@ class GradModel(abc.ABC):
 
                 # Output focusing section head.
                 section, inkey, outkey = grouped[0]
-                debug(
-                    "\" {:s} {:s} | \033[34{highlight:s}m{:s}\033[0m ==>" \
-                    " \033[32{highlight:s}m{:s}\033[0m \".",
-                    subname.rjust(max_sub), section.rjust(max_sec),
-                    inkey.rjust(max_in), outkey.ljust(max_out),
-                    highlight=highlight,
-                )
+                if (len(inkey) == 0 or len(outkey) == 0):
+                    debug(
+                        "\" {:s} {:s} | \033[34{highlight:s}m{:s}\033[0m" \
+                        "     \033[32{highlight:s}m{:s}\033[0m \".",
+                        subname.rjust(max_sub), section.rjust(max_sec),
+                        inkey.rjust(max_in), outkey.ljust(max_out),
+                        highlight=highlight,
+                    )
+                else:
+                    debug(
+                        "\" {:s} {:s} | \033[34{highlight:s}m{:s}\033[0m ==>" \
+                        " \033[32{highlight:s}m{:s}\033[0m \".",
+                        subname.rjust(max_sub), section.rjust(max_sec),
+                        inkey.rjust(max_in), outkey.ljust(max_out),
+                        highlight=highlight,
+                    )
 
                 # Output remaining focusing section.
                 for section, inkey, outkey in grouped[1:]:
@@ -745,6 +751,41 @@ class GradModel(abc.ABC):
                     "\"-{:s}-{:s}-+-{:s}-----{:s}-\".",
                     "-" * max_sub, "-" * max_sec, "-" * max_in, "-" * max_out,
                 )
+
+    def register(
+        self: GradModel,
+        *args: ArgT,
+        **kargs: KArgT,
+    ) -> None:
+        r"""
+        Register parameters and sub models.
+
+        Args
+        ----
+        - self
+        - *args
+        - **kargs
+
+        Returns
+        -------
+
+        """
+        # Scan attribtues for parameters and sub models.
+        self.parameter = Parameter()
+        for key, val in vars(self).items():
+            if (isinstance(val, torch.nn.parameter.Parameter)):
+                self.parameter.registar_parameter(key, val)
+            elif (isinstance(val, GradModel)):
+                if (val.SUB):
+                    self.parameter.registar_submodel(key, val.parameter)
+                else:
+                    error(
+                        "Register a non-sub model \"{:s}\" as a sub model.",
+                        val.__class__.__name__,
+                    )
+                    raise RuntimeError
+            else:
+                pass
 
     @abc.abstractmethod
     def configure(

pytorch/models/seq/gru.py
@@ -585,7 +585,7 @@ class __GRU__(GRU):
 
             # Get essential recurrent transient buffer and expand batch
             # dimension by batch size.
-            tensor = self.cell_hsub.nullout(device)[self.ky_cell_h]
+            tensor = self.cell_hsub_nullout(device)[self.ky_cell_h]
             shape = list(tensor.size())[1:]
             transient = tensor.expand(batch_size, *shape)
 
@@ -631,6 +631,9 @@ class __GRU__(GRU):
         self.bias_ih: torch.nn.parameter.Parameter
         self.bias_hh: torch.nn.parameter.Parameter
 
+        # Super.
+        GRU.configure(self, xargs, xkargs)
+
         # Save necessary attributes.
         self.num_inputs = xkargs["num_inputs"]
         self.num_outputs = xkargs["num_outputs"]
@@ -654,8 +657,11 @@ class __GRU__(GRU):
         self.cell_isub = xkargs["cell_isub"]
         self.cell_hsub = xkargs["cell_hsub"]
 
-        # Pre-fetch IO direction only for linear form.
+        # Pre-fetch IO direction for consistency.
         (self.ky_input_i,), (_,) = self.cell_isub.IOKEYS["linear"]
+
+        # Pre-fetch null function for consistency.
+        self.cell_hsub_nullout = self.cell_hsub.nullout
 
     def __initialize__(
         self: __GRU__,
@@ -688,58 +694,29 @@ class __GRU__(GRU):
         # /
         ...
 
-        # Simulate linear units.
-        self.reset_isub = cast(Linear, self.reset_isub)
-        self.reset_hsub = cast(Linear, self.reset_hsub)
-        self.update_isub = cast(Linear, self.update_isub)
-        self.update_hsub = cast(Linear, self.update_hsub)
-        self.cell_isub = cast(Linear, self.cell_isub)
-        self.cell_hsub = cast(Linear, self.cell_hsub)
-        self.reset_isub.initialize(
-            rng,
-            xargs=xkargs["gate"]["xargs"], xkargs=xkargs["gate"]["xkargs"],
-        )
-        self.reset_hsub.initialize(
-            rng,
-            xargs=xkargs["gate"]["xargs"], xkargs=xkargs["gate"]["xkargs"],
-        )
-        self.update_isub.initialize(
-            rng,
-            xargs=xkargs["gate"]["xargs"], xkargs=xkargs["gate"]["xkargs"],
-        )
-        self.update_hsub.initialize(
-            rng,
-            xargs=xkargs["gate"]["xargs"], xkargs=xkargs["gate"]["xkargs"],
-        )
-        self.cell_isub.initialize(
-            rng,
-            xargs=xkargs["cell"]["xargs"], xkargs=xkargs["cell"]["xkargs"],
-        )
-        self.cell_hsub.initialize(
-            rng,
-            xargs=xkargs["cell"]["xargs"], xkargs=xkargs["cell"]["xkargs"],
-        )
+        # Super.
+        GRU.__initialize__(self, rng, xargs=xargs, xkargs=xkargs)
 
         # Merge matrices to fit PyTorch.
         weight_ih = getattr(torch, "cat")([
-            self.reset_isub.weight.data,
-            self.update_isub.weight.data,
-            self.cell_isub.weight.data,
+            cast(Linear, self.reset_isub).weight.data,
+            cast(Linear, self.update_isub).weight.data,
+            cast(Linear, self.cell_isub).weight.data,
         ], dim=0)
         weight_hh = getattr(torch, "cat")([
-            self.reset_hsub.weight.data,
-            self.update_hsub.weight.data,
-            self.cell_hsub.weight.data,
+            cast(Linear, self.reset_hsub).weight.data,
+            cast(Linear, self.update_hsub).weight.data,
+            cast(Linear, self.cell_hsub).weight.data,
         ], dim=0)
         bias_ih = getattr(torch, "cat")([
-            self.reset_isub.bias.data,
-            self.update_isub.bias.data,
-            self.cell_isub.bias.data,
+            cast(Linear, self.reset_isub).bias.data,
+            cast(Linear, self.update_isub).bias.data,
+            cast(Linear, self.cell_isub).bias.data,
         ], dim=0)
         bias_hh = getattr(torch, "cat")([
-            self.reset_hsub.bias.data,
-            self.update_hsub.bias.data,
-            self.cell_hsub.bias.data,
+            cast(Linear, self.reset_hsub).bias.data,
+            cast(Linear, self.update_hsub).bias.data,
+            cast(Linear, self.cell_hsub).bias.data,
         ], dim=0)
 
         # Copy data to correct place.
@@ -758,3 +735,9 @@ class __GRU__(GRU):
         del self.parameter.submodels["update_hsub"]
         del self.parameter.submodels["cell_isub"]
         del self.parameter.submodels["cell_hsub"]
+        del self.reset_isub
+        del self.reset_hsub
+        del self.update_isub
+        del self.update_hsub
+        del self.cell_isub
+        del self.cell_hsub

pytorch/models/seq/rnn.py
@@ -508,7 +508,7 @@ class __RNN__(RNN):
 
             # Get essential recurrent transient buffer and expand batch
             # dimension by batch size.
-            tensor = self.hsub.nullout(device)[self.ky_aggin_h]
+            tensor = self.hsub_nullout(device)[self.ky_aggin_h]
             shape = list(tensor.size())[1:]
             transient = tensor.expand(batch_size, *shape)
 
@@ -554,11 +554,13 @@ class __RNN__(RNN):
         self.bias_ih: torch.nn.parameter.Parameter
         self.bias_hh: torch.nn.parameter.Parameter
 
+        # Super.
+        RNN.configure(self, xargs, xkargs)
+
         # Save necessary attributes.
         self.num_inputs = xkargs["num_inputs"]
         self.num_outputs = xkargs["num_outputs"]
         self.no_bias = xkargs["no_bias"]
-        self.transform_name = xkargs["transform"]
 
         # Allocate parameters.
         self.pytorch = torch.nn.RNNCell(
@@ -574,8 +576,11 @@ class __RNN__(RNN):
         self.isub = xkargs["isub"]
         self.hsub = xkargs["hsub"]
 
-        # Pre-fetch IO direction only for linear form.
+        # Pre-fetch IO direction for consistency.
         (self.ky_input_i,), (_,) = self.isub.IOKEYS["linear"]
+
+        # Pre-fetch null function for consistency.
+        self.hsub_nullout = self.hsub.nullout
 
     def __initialize__(
         self: __RNN__,
@@ -608,21 +613,20 @@ class __RNN__(RNN):
         # /
         ...
 
-        # Simulate linear units.
-        self.isub = cast(Linear, self.isub)
-        self.hsub = cast(Linear, self.hsub)
-        self.isub.initialize(rng, xargs=xargs, xkargs=xkargs)
-        self.hsub.initialize(rng, xargs=xargs, xkargs=xkargs)
+        # Super.
+        RNN.__initialize__(self, rng, xargs=xargs, xkargs=xkargs)
 
         # Copy data to correct place.
-        self.weight_ih.data.copy_(self.isub.weight.data)
-        self.weight_hh.data.copy_(self.hsub.weight.data)
+        self.weight_ih.data.copy_(cast(Linear, self.isub).weight.data)
+        self.weight_hh.data.copy_(cast(Linear, self.hsub).weight.data)
         if (self.no_bias):
             pass
         else:
-            self.bias_ih.data.copy_(self.isub.bias.data)
-            self.bias_hh.data.copy_(self.hsub.bias.data)
+            self.bias_ih.data.copy_(cast(Linear, self.isub).bias.data)
+            self.bias_hh.data.copy_(cast(Linear, self.hsub).bias.data)
 
         # Remove from registration.
         del self.parameter.submodels["isub"]
         del self.parameter.submodels["hsub"]
+        del self.isub
+        del self.hsub